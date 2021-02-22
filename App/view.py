4ad197bf79a65ed1fd2e00b425c7a1d36150ae87
Julipa02 <j.parraf@uniandes.edu.co>
2021-02-21 21:17:14 -0500

    Entrega Ejemplo - laboratorio 4

--- App/view.py
+++ App/view.py
@@ -82,6 +82,17 @@ def printBestBooks(books):
 
 catalog = None
 
+def printResults(ord_books, sample=10):
+    size = lt.size(ord_books)
+    if size > sample:
+        print("Los primeros ", sample, " libros ordenados son:")
+        i=0
+        while i <= sample:
+            book = lt.getElement(ord_books,i)
+            print('Titulo: ' + book['title'] + ' ISBN: ' +
+                  book['isbn'] + ' Rating: ' + book['average_rating'])
+            i+=1
+
 """
 Menu principal
 """
@@ -121,14 +132,4 @@ while True:
     else:
         sys.exit(0)
 
-def printResults(ord_books, sample=10):
-    size = lt.size(ord_books)
-    if size > sample:
-        print("Los primeros ", sample, " libros ordenados son:")
-        i=0
-        while i <= sample:
-            book = lt.getElement(ord_books,i)
-            print('Titulo: ' + book['title'] + ' ISBN: ' +
-                  book['isbn'] + ' Rating: ' + book['average_rating'])
-            i+=1
 sys.exit(0)
